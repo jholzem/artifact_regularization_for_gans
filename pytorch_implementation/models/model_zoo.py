f67c8322c7220601483fa757e01c7d21862cfebb
jholzem <jholzem@student.ethz.ch>
2020-12-11 12:04:48 +0100

    Initial commit

---
+++ pytorch_implementation/models/model_zoo.py
@@ -0,0 +1,14 @@
+# python3.7
+"""Model zoo."""
+
+# pylint: disable=line-too-long
+
+MODEL_ZOO = {
+    'stylegan_ffhq256': dict(
+        gan_type='stylegan',
+        resolution=256,
+        url='https://mycuhk-my.sharepoint.com/:u:/g/personal/1155082926_link_cuhk_edu_hk/ES-NAUCC2qdHg87BftvlBiQBVpbJ8-005Q4TNr5KrOxQEw?e=00AnWt&download=1',
+    ),
+}
+
+# pylint: enable=line-too-long
