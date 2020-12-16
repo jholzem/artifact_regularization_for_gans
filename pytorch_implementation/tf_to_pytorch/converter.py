7ddee0e7c50bfb78338a0d3e4a63f519a63198a9
jholzem <jholzem@student.ethz.ch>
2020-12-16 12:04:02 +0100

    Added .csv files for model matching

--- pytorch_implementation/tf_to_pytorch/converter.py
+++ pytorch_implementation/tf_to_pytorch/converter.py
@@ -5,8 +5,11 @@ import torch
 import collections
 from collections import OrderedDict
 
+"""
 dnnlib.tflib.init_tf()
 _, _, D, _ = pickle.load(open('./styleganinv_face_256.pkl', 'rb'))
-weights_pt = collections.OrderedDict([(k, torch.from_numpy(v.value().eval())) \
-                                       for k, v in D.trainables.items()])
+weights_pt = collections.OrderedDict((k, torch.from_numpy(v.value().eval())) \
+                                       for k, v in D.trainables.items())
+"""
 
+weights = torch.load('./weights/styleganinv_face_256_discriminator.pt')[0]
