a3a89a782bbc5f0c677d986e9c8e1828b86b5551
Max <schaller.bike@gmail.com>
2020-12-28 16:55:57 +0100

    Update realZfake.py

--- genforce/realZfake.py
+++ genforce/realZfake.py
@@ -48,8 +48,8 @@ for i in range(2):
     # create optimized latent code & fake images
     for k in range(real.shape[0]):
         latent, fake, loss = Inverter.invert_offline(image=(real[k].type(torch.cuda.FloatTensor)).unsqueeze(0))
-        latents.append(latent.squeeze().detach().numpy())
-        fakes.append(fake.squeeze().detach().numpy())
+        latents.append(latent.squeeze().detach().cpu().numpy())
+        fakes.append(fake.squeeze().detach().cpu().numpy())
         losses.append(loss)
 
         print('ETA:', (11000-counter)*(time.time()-start)/counter / 3600, 'hours')
