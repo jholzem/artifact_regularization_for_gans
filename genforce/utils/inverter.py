8294f27261954fc95d6b17211d4f421ab94e86b2
Max <schaller.bike@gmail.com>
2020-12-28 16:41:34 +0100

    Update inverter.py

--- genforce/utils/inverter.py
+++ genforce/utils/inverter.py
@@ -281,7 +281,7 @@ class StyleGANInverter(object):
     x.requires_grad = False
     init_z = self.get_init_code(image)
 
-    z = torch.Tensor(init_z)
+    z = torch.cuda.Tensor(init_z)#torch.Tensor(init_z)
     x_rec = self.G.net.synthesis(z)#self.G.net.module.synthesis(z)#
     if self.save:
       save_path = os.path.join(self.save_dir, str(self.iteration_counter), "enc.png")
