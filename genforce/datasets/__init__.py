13110468bd27acca9137c27aa6455398f18bfe05
Max <schaller.bike@gmail.com>
2020-12-30 12:06:55 +0100

    Update __init__.py

--- genforce/datasets/__init__.py
+++ genforce/datasets/__init__.py
@@ -2,6 +2,7 @@
 """Collects datasets and data loaders."""
 
 from .datasets import BaseDataset
+from .datasets import BaseDatasetWithLatent
 from .dataloaders import IterDataLoader
 
-__all__ = ['BaseDataset', 'IterDataLoader']
+__all__ = ['BaseDataset', 'BaseDatasetWithLatent', 'IterDataLoader']
