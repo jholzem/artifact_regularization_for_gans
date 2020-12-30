95481af348c97227314274096bcf3950d7d40914
jholzem <jholzem@ethz.ch>
2020-12-30 11:17:50 +0100

    Debugged version for leonhard cluster

--- genforce/scripts/dist_train.sh
+++ genforce/scripts/dist_train.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-GPUS=$1
-CONFIG=$2
-WORK_DIR=$3
+#GPUS=$1
+#CONFIG=$2
+#WORK_DIR=$3
 PORT=${PORT:-29500}
+GPUS=1
+CONFIG=configs/stylegan_ffhq256_fourier_regularized.py
+WORK_DIR=work_dirs/stylegan_ffhq256_f_reg_train
 
 python -m torch.distributed.launch \
        --nproc_per_node=${GPUS} \
@@ -11,4 +14,4 @@ python -m torch.distributed.launch \
        ./train.py ${CONFIG} \
            --work_dir ${WORK_DIR} \
            --launcher="pytorch" \
-           ${@:4}
+	   ${@:4}
