29db0bc27dd8c2aced895448a137aedf4920bef9
steffeol <steffeol@ethz.ch>
2020-12-30 14:44:12 +0100

    test eval

--- genforce/scripts/eval.sh
+++ genforce/scripts/eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python img_syn.py 10 'styleganinv_ffhq256'
+python img_syn.py 1000 'styleganinv_ffhq256'
 
 python demo_dir.py -d img_syn_genforce/styleganinv_ffhq256 -m weights/blur_jpg_prob0.1.pth
