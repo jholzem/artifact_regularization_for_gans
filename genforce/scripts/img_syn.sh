28e03858e105fe46be705db9acad0ea6348ad4dc
steffeol <steffeol@ethz.ch>
2020-12-30 22:01:33 +0100

    test_syn

--- genforce/scripts/img_syn.sh
+++ genforce/scripts/img_syn.sh
@@ -5,4 +5,4 @@ GENERATOR=$2
 
 python img_syn.py ${N_IMAGES} ${GENERATOR}
 
-python demo_dir.py -d img_syn_genforce/${GENERATOR} -m weights/blur_jpg_prob0.1.pth
+python demo_dir.py -d test_syn -m weights/blur_jpg_prob0.1.pth
