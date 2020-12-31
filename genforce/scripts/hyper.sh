f77436523adb3cf5f94082c3694e31ac2b46de47
Max <schaller.bike@gmail.com>
2020-12-31 09:59:19 +0100

    Update hyper.sh

--- genforce/scripts/hyper.sh
+++ genforce/scripts/hyper.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 
-LAMBDAS={"1","5e-1"}
+LAMBDAS="1 5e-1"
 METRIC=cos
-LRS={"1e-3","1e-4"}
+LRS="1e-3 1e-4"
 NETHZ=mschaller
 
 
-for LAMBDA in LAMBDAS;
+for LAMBDA in $LAMBDAS;
 
 do
 
-    for LR in LRS;
+    for LR in $LRS;
 
     do
 
