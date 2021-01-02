b53c1dcafe1ead8a4ae41cc8953a8e2f98eb5d97
Max <schaller.bike@gmail.com>
2021-01-02 19:03:18 +0100

    removed scripts and configs folder from genforce

---
+++ scripts/train_eval.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+LAMB=$1
+METRIC=$2
+BASELR=$3
+N_IMAGES=$4
+NETHZ=$5
+PORT=$RANDOM #{PORT:-29500}
+
+BAR="_"
+SAVENAME="${LAMB}$BAR${METRIC}$BAR${BASELR}"
+FOLDER="/cluster/scratch/"
+RES="/results/"
+ENDING="_generator.pth"
+TEMP="_workdir"
+
+SYNFOLDER="$FOLDER${NETHZ}$RES$SAVENAME"
+WORK_DIR="$FOLDER${NETHZ}$RES$SAVENAME$TEMP"
+
+GPUS=1
+CONFIG=genforce/configuration.py
+
+python -m torch.distributed.launch \
+       --nproc_per_node=${GPUS} \
+       --master_port=${PORT} \
+       ./train.py ${CONFIG} \
+           --work_dir ${WORK_DIR} \
+           --launcher="pytorch" \
+           --lamb=${LAMB} \
+           --metric=${METRIC} \
+           --baseLR=${BASELR} \
+           --nethz=${NETHZ}
+	   ${@:8}
+
+
+python img_syn.py ${N_IMAGES} "$FOLDER${NETHZ}$RES$SAVENAME$BAR$idx$ENDING" ${SYNFOLDER}
+python demo_dir.py -d ${SYNFOLDER} -m weights/blur_jpg_prob0.1.pth
+
+rm -r ${SYNFOLDER}
