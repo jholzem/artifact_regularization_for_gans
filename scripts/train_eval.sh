75fa01d0d5964cf47100a825fa2f6e0a7b67ebc9
jholzem <75212024+jholzem@users.noreply.github.com>
2021-01-03 14:00:22 +0100

    Update train_eval.sh

--- scripts/train_eval.sh
+++ scripts/train_eval.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
 LAMB=$1
 METRIC=$2
 BASELR=$3
 N_IMAGES=$4
 NETHZ=$5
-PORT=$RANDOM #{PORT:-29500}
+PORT=$RANDOM
 
 EPOCHS=1
 
@@ -36,6 +34,6 @@ python -m torch.distributed.launch \
 
 
 python img_syn.py ${N_IMAGES} "$FOLDER${NETHZ}$RES$SAVENAME$BAR$EPOCHS$ENDING" ${SYNFOLDER}
-python ./CNNDetection/demo_dir.py -d ${SYNFOLDER} -m ./weights/blur_jpg_prob0.1.pth
+python ./CNNDetection/detection.py -d ${SYNFOLDER} -m ./weights/blur_jpg_prob0.1.pth
 
 rm -r ${SYNFOLDER}
