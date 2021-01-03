7823d961dac7ac442057723735a5984bc17ee17e
jholzem <75212024+jholzem@users.noreply.github.com>
2021-01-03 14:01:23 +0100

    Update download.sh
    Removed unnecessary download for CNNDetection

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # weights for CNNDetection
-wget https://www.dropbox.com/s/2g2jagq2jn1fd0i/blur_jpg_prob0.5.pth?dl=0 -O CNNDetection/weights/blur_jpg_prob0.5.pth
 wget https://www.dropbox.com/s/h7tkpcgiwuftb6g/blur_jpg_prob0.1.pth?dl=0 -O CNNDetection/weights/blur_jpg_prob0.1.pth
 
 # weights for StyleGAN
